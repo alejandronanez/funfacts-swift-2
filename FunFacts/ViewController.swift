fb90eba97a19391d0700a9c40e1e9b37d3ff439c
Alejandro Nanez <alejonanez@gmail.com>
2016-07-23 18:00:27 -0500

    Initial Commit

---
+++ FunFacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by Alejo Nanez Ortiz on 7/23/16.
+//  Copyright © 2016 Alejandro Nanez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
