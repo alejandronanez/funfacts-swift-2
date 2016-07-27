ed336b46d03076ef2065fd1fc284664063c85813
Alejandro Nanez <alejonanez@gmail.com>
2016-07-26 20:17:08 -0500

    Work in progress

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var funFactLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        funFactLabel.text = "Other thing"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func showFunFact() {
+        funFactLabel.text = "Another thing"
+    }
+ 
 }
 
